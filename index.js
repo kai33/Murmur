b176f055b747e259e58ebe2d14de4770567959e7
Kai33 <gigikie@gmail.com>
2014-02-21 23:31:38 +0800

    add: express.static for ./public

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
-require(__dirname + '/route')(app)
-//TODO:
-//get & post preprocess/filter!
+require(__dirname + '/route')(app, express)
 app.listen(3000)
+
+//TODO:
+//get & post preprocess/postprocess filter!
--- route.js
+++ route.js
@@ -1,31 +1,39 @@
-var fs = require('fs'),
+var fileSystem = require('fs'),
     path = require('path'),
     controllerDirectory = __dirname + '/controller',
-    //two special: home and _404
     home = require(controllerDirectory + '/home'),
     _404 = require(controllerDirectory + '/_404'),
     controllerName,
     controllerPath,
     controller
 
-module.exports = function(app){
-    //handle homepage
+module.exports = function(app, express){
+    //set up public folder
+    app.use(express.static('./public'));
+    //set up homepage
     app.get('/', home)
-    //handle other controllers
-    fs.readdir(controllerDirectory, function(err, files){
+    //set up other controllers
+    fileSystem.readdir(controllerDirectory, function(err, files){
         if(!err){
-            files.forEach(function(item){
-                //eg for item home.js -> /home
-                controllerName = '/' + path.basename(item, '.js')
-                //eg for /home -> ./controller/home
-                controllerPath = controllerDirectory + controllerName
-                //set up get & post for controllers
-                controller  = require(controllerPath)
-                app.get(controllerName, controller)
-                app.post(controllerName, controller)
-            })
+            setupControllers(app, files);
         }
-        //handle default
+        //set up default case with _404
         app.get('*', _404)
     })
 }
+
+function setupControllers(app, files) {
+    files.forEach(function (item) {
+        //eg for item home.js -> /home
+        controllerName = '/' + path.basename(item, '.js')
+        //eg for /home -> ./controller/home
+        controllerPath = controllerDirectory + controllerName
+        //set up get & post for controllers
+        controller = require(controllerPath)
+        app.get(controllerName, controller)
+        app.post(controllerName, controller)
+    })
+}
+
+//TODO:
+//cache
